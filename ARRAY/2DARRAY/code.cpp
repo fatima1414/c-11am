b42a32c297a7d8711ff6cd4605f44ac846e5b0ce
fatima1414 <shaikhfatima5260@gmai.com>
2025-01-24 14:17:11 +0530

    code

--- ARRAY/2DARRAY/code.cpp
+++ ARRAY/2DARRAY/code.cpp
@@ -1,13 +1,14 @@
 // cout code of ARRAY
 
- #include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int row_size = 3;
     int col_size = 4;
     int arr[row_size][col_size];
-// manually input
-    arr[0][0] = 11 ;
+    // manually input
+    arr[0][0] = 11;
     arr[0][1] = 22;
     arr[0][2] = 44;
     arr[0][3] = 45;
@@ -16,27 +17,61 @@ int main(){
     arr[1][2] = 58;
     arr[1][3] = 79;
     arr[2][0] = 47;
-    arr[2][1] =58 ;
-    arr[2][2] =79 ;
-    arr[2][3] =20 ;
-// loop -> print ->2d array
-// foreach -> not ok
-    // for(int element : arr){
-    //     cout<<element<<endl;
-    // }
-    // output
-
-    for(int i = 0; i < row_size; i++){
-        for(int j = 0; j < col_size; j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    arr[2][1] = 58;
+    arr[2][2] = 79;
+    arr[2][3] = 20;
+    // manually print
+    cout << arr[0][0] << endl;
+    cout << arr[0][1] << endl;
+    cout << arr[0][2] << endl;
+    cout << arr[0][3] << endl;
+    cout << arr[1][0] << endl;
+    cout << arr[1][1] << endl;
+    cout << arr[1][2] << endl;
+    cout << arr[1][3] << endl;
+    cout << arr[2][0] << endl;
+    cout << arr[2][1] << endl;
+    cout << arr[2][2] << endl;
+    cout << arr[2][3] << endl;
 }
 
+// //  #include<iostream>
+// // using namespace std;
+// // int main(){
+// //     int row_size = 3;
+// //     int col_size = 4;
+// //     int arr[row_size][col_size];
+// // // manually input
+// //     arr[0][0] = 11 ;
+// //     arr[0][1] = 22;
+// //     arr[0][2] = 44;
+// //     arr[0][3] = 45;
+// //     arr[1][0] = 14;
+// //     arr[1][1] = 63;
+// //     arr[1][2] = 58;
+// //     arr[1][3] = 79;
+// //     arr[2][0] = 47;
+// //     arr[2][1] =58 ;
+// //     arr[2][2] =79 ;
+// //     arr[2][3] =20 ;
 
-  //USER DEFINE (CIN CODE OF ARRY)
-  
+// // // loop -> print ->2d array
+// // // foreach -> not ok
+// //     // for(int element : arr){
+// //     //     cout<<element<<endl;
+// //     // }
+// //     // output
+
+// //     for(int i = 0; i < row_size; i++){
+// //         for(int j = 0; j < col_size; j++){
+// //             cout<<arr[i][j]<<" ";
+// //         }
+// //         cout<<endl;
+// //     }
+// }
+
+// USER DEFINE (CIN CODE OF ARRY)
+
 // #include<iostream>
 // using namespace std;
 // int main(){
