d70d1a8f4288fb252f50ce497c6f4b879614c82b
fatima1414 <shaikhfatima5260@gmai.com>
2025-01-29 13:29:25 +0530

    code

---
+++ POINTER/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;    // variable declared
+    int *ptr = &a; // pointer variable declared and assign address of a.
+                   // value
+    cout << "a : " << a << endl;
+    cout << "*ptr : " << *ptr << endl;
+    // address
+    cout << "Address of a : " << &a << endl;
+    cout << "Address of ptr : " << ptr << endl;
+}
