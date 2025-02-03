4dfb41540348598a1ea2161e9e58089ae77fee14
fatima1414 <shaikhfatima5260@gmai.com>
2025-02-03 12:54:00 +0530

    code

---
+++ OOPS/code.cpp
@@ -0,0 +1,64 @@
+// oops
+  
+//   syntax of class
+
+// #include <iostream>
+// using namespace std;
+// // create class
+// class Person
+// {
+// };
+// int main()
+// {
+//     Person fatima; // create object
+// }
+
+// public access
+
+// #include<iostream>
+// using namespace std;
+// // create class
+// class Person{
+//     // Access specifier
+//     public :
+//     string name;
+//     int age;
+//     // method
+//     void display(){
+//         cout<<"Name : "<<name<<endl;
+//         cout<<"Age : "<<age<<endl;
+//     }
+// };
+// int main(){
+//     Person f;  // create object
+// // insilization -> outide of class
+//     f.name = "Fatima Ji";
+//     f.age = 11;
+//     f.display();
+// }
+
+
+// private & protected is outside call is not ok
+
+// #include<iostream>
+// using namespace std;
+// // create class
+// class Person{
+//     // Access specifier
+//     // private :
+//     protected :
+//     string name;
+//     int age;
+//     // method
+//     void display(){
+//         cout<<"Name : "<<name<<endl;
+//         cout<<"Age : "<<age<<endl;
+//     }
+// };
+// int main(){
+//     Person f;  // create object
+// // insilization -> outide of class
+//     f.name = "Fatima Ji";
+//     f.age = 11;
+//     f.display();
+// }
