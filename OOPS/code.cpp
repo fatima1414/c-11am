79b2fbad4cc91d3e46664e034c26214e24231700
fatima1414 <shaikhfatima5260@gmai.com>
2025-02-04 10:57:12 +0530

    code

--- OOPS/code.cpp
+++ OOPS/code.cpp
@@ -1,5 +1,5 @@
 // oops
-  
+
 //   syntax of class
 
 // #include <iostream>
@@ -15,28 +15,51 @@
 
 // public access
 
-// #include<iostream>
+#include<iostream>
+using namespace std;
+// create class
+class Person{
+    // Access specifier
+    public :
+
+    //data member
+    string name;
+    int age;
+
+    // method
+    void display(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age : "<<age<<endl;
+    }
+};
+int main(){
+    Person f;  // create object
+// insilization -> outide of class
+    f.name = "Fatima";
+    f.age = 11;
+    f.display();
+}
+
+// #include <iostream>
 // using namespace std;
-// // create class
-// class Person{
-//     // Access specifier
-//     public :
-//     string name;
-//     int age;
-//     // method
-//     void display(){
-//         cout<<"Name : "<<name<<endl;
-//         cout<<"Age : "<<age<<endl;
+// class Car
+// {
+// public:
+//     string brand; // Public member
+
+//     void display()
+//     {
+//         cout << "Car Brand: " << brand << endl;
 //     }
 // };
-// int main(){
-//     Person f;  // create object
-// // insilization -> outide of class
-//     f.name = "Fatima Ji";
-//     f.age = 11;
-//     f.display();
-// }
 
+// int main()
+// {
+//     Car car1;
+//     car1.brand = "BMW"; // ✅ Directly modifying public member
+//     car1.display();
+//     return 0;
+// }
 
 // private & protected is outside call is not ok
 
---
+++ OOPS/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ // Car car1;
+    // car1.brand = "BMW"; // ✅ Directly modifying public member
+    // car1.display();
+    // return 0;
