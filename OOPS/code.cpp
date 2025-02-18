d1b8735464670a6c800172613477e111278c3f92
fatima1414 <shaikhfatima5260@gmai.com>
2025-02-18 13:26:01 +0530

    code

--- OOPS/code.cpp
+++ OOPS/code.cpp
@@ -891,3 +891,28 @@
 //     Complex c3 = c1*c2;
 //     c3.getComplex();
 // }
+
+
+ // run-time polymorphism
+
+#include<iostream>
+using namespace std;
+class Parent{
+    public :
+   virtual void display(){
+        cout<<"Parrent Class"<<endl;
+    }
+};
+class Child:public Parent{
+    public:
+    void display() override {
+        cout<<"Child Class"<<endl;
+    }
+};
+int main(){
+    // Parent p;
+    // p.display();
+    Child ch;
+    ch.display();
+    ch.Parent::display();
+}
