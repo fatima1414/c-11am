eaafe747ace134dc75d32a147b234b0d0597cddb
fatima1414 <shaikhfatima5260@gmai.com>
2025-02-21 14:37:55 +0530

    code

--- OOPS/code.cpp
+++ OOPS/code.cpp
@@ -1025,3 +1025,121 @@
 
 //     // sh.showDisplay();
 // }
+
+
+// copy constructor
+
+// #include <iostream>
+// using namespace std;
+
+// class Student
+// {
+// public:
+//     string *name;
+//     int age;
+
+//     Student(string n, int a)
+//     {
+//         this->name = new string(n);
+      
+//         this->age = a;
+//     }
+
+//     // copy constructor
+
+//     Student( Student &other)
+//     {
+//        name = other.name;
+//         age = other.age;
+//     }
+
+//     ~Student(){
+//         cout << "Distructor called..." << endl;
+
+//         delete name;
+
+//         cout << "Name : " << *name << endl;
+//     }
+
+//     void display()
+//     {
+//         cout << "I am " << *name << ". My age is " << age << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Student s1("Vraj", 24); // parameterized constructor
+
+//     Student s2 = s1; // copy constructor
+
+//     s1.display();
+
+//     s2.display();
+
+//     cout << "Updated data getting..." << endl;
+
+//     *s2.name = "Prakash";
+
+//     s1.display();
+//     s2.display();
+// }
+
+
+ // Deep copy constructor
+
+// #include <iostream>
+// using namespace std;
+
+// class Student
+// {
+// public:
+//     string *name;
+//     int age;
+
+//     Student(string n, int a)
+//     {
+//         this->name = new string(n);
+      
+//         this->age = a;
+//     }
+
+//     // copy constructor
+
+//     Student(const Student &other) 
+//     {
+//        this->name = new string (*other.name);
+//        this->age = other.age;
+//     }
+
+//     ~Student(){
+//         cout << "Distructor called..." << endl;
+
+//         delete name;
+
+//         cout << "Name : " << *name << endl;
+//     }
+
+//     void display()
+//     {
+//         cout << "I am " << *name << ". My age is " << age << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Student s1("Vraj", 24); // parameterized constructor
+
+//     Student s2 = s1; // copy constructor
+
+//     s1.display();
+
+//     s2.display();
+
+//     cout << "Updated data getting..." << endl;
+
+//     *s2.name = "Prakash";
+
+//     s1.display();
+//     s2.display();
+// }
--- OOPS/tempCodeRunnerFile.cpp
+++ OOPS/tempCodeRunnerFile.cpp
@@ -1,14 +1,6 @@
 
-#include<iostream>
-using namespace std;
-class Teacher{
-    public :
-      static double salary;  // class level variable
-  static void getDetails(){
-        cout<<"Salary : "<<salary<<endl;
-    }
-};
-double Teacher::salary = 99;  // static data member initialiston
-int main(){
-    Teacher::getDetails();
-}
+    // Student( Student &other)
+    // {
+    //    name = other.name;
+    //     age = other.age;
+    // }
