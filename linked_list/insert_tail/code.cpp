1e6df5be7c802f4193d97e9b75b8ff74afc21d9c
fatima1414 <shaikhfatima5260@gmai.com>
2025-03-06 10:50:58 +0530

    code

--- linked_list/insert_tail/code.cpp
+++ linked_list/insert_tail/code.cpp
@@ -1,66 +1,185 @@
 // insert At Tail
-#include<iostream>
-using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node *next;
-                      //craeting a node
-    Node(int data){
-        this->data = data;
-        this->next = NULL;
-    }
-};   
+// #include<iostream>
+// using namespace std;
 
-void insertAtTail(Node* &head, int data){
-    Node *newNode = new Node(data);  // ready newNode
+// class Node{
+//     public:
+//         int data;
+//         Node *next;//point addres
+//         //craeting a node
+//     Node(int data){
+//         this->data = data;
+//         this->next = NULL;
+//     }
+// };   
 
-    if(head == NULL){
-        head = newNode;
-        return;
-    }
+// // inserat a tail(//craeting a NEW node)
+// void insertAtTail(Node* &head, int data){
+//     Node *newNode = new Node(data);  // ready newNode
 
-    Node *temp = head;
+//     if(head == NULL){
+//         head = newNode;
+//         return;
+//     }
 
-    while(temp->next != NULL){
-        temp = temp->next;
-    }
+//     Node *temp = head;
 
-    temp->next = newNode;
-}
+//     while(temp->next != NULL){
+//         temp = temp->next;
+//     }
 
-void display(Node *&head)
-{
-    if(head == NULL){
-        cout << "LinkedList is Empty!" << endl;
-        return;
-    }
+//     temp->next = newNode;
+// }
 
-    Node *temp = head;
+// void display(Node *&head)
+// {
+//     if(head == NULL){
+//         cout << "LinkedList is Empty!" << endl;
+//         return;
+//     }
 
-    while(temp != NULL){
-        cout << temp->data << "->";
-        temp = temp->next;
-    }
+//     Node *temp = head;
 
-    cout << "NULL" << endl;
+//     while(temp != NULL){
+//         cout << temp->data << "->";
+//         temp = temp->next;
+//     }
+
+//     cout << "NULL" << endl;
 
     
-}
-int main(){
+// }
+// int main(){
 
-    Node *head = NULL;
+//     Node *head = NULL;
 
-    insertAtTail(head, 10);
-    insertAtTail(head, 20);
+//     insertAtTail(head, 10);
+//     insertAtTail(head, 20);
 
-    // display(head);
+//     // display(head);
 
-    insertAtTail(head, 30);
-    insertAtTail(head, 40);
-    insertAtTail(head, 50);
-    insertAtTail(head, 60);
+//     insertAtTail(head, 30);
+//     insertAtTail(head, 40);
+//     insertAtTail(head, 50);
+//     insertAtTail(head, 60);
 
-    display(head);
+//     display(head);
+// }
+
+// //CHATGPT CODE
+// #include <iostream>
+// using namespace std;
+
+// class Node {
+// public:
+//     int data;
+//     Node* next;
+
+//     Node(int val) { // Constructor (jab bhi naya node banaye)
+//         data = val;
+//         next = nullptr;
+//     }
+// };
+
+// class LinkedList {
+// public:
+//     Node* head;
+
+//     LinkedList() { // Constructor (pehle head NULL hoga)
+//         head = nullptr;
+//     }
+
+//     void insertAtTail(int val) { // Last me insert karne ka function
+//         Node* newNode = new Node(val); // Naya node banaya
+//         if (head == nullptr) {  // Agar list khaali hai
+//             head = newNode;
+//             return;
+//         }
+
+//         Node* temp = head; // Head se start karenge
+//         while (temp->next != nullptr) { // Last node tak jayenge
+//             temp = temp->next;
+//         }
+//         temp->next = newNode; // Last node ke next me naya node daal diya
+//     }
+
+//     void display() { // List ko print karne ka function
+//         Node* temp = head;
+//         while (temp != nullptr) {
+//             cout << temp->data << " -> ";
+//             temp = temp->next;
+//         }
+//         cout << "NULL" << endl;
+//     }
+// };
+
+// int main() {
+//     LinkedList list;
+//     list.insertAtTail(10);
+//     list.insertAtTail(20);
+//     list.insertAtTail(30);
+
+//     list.display(); // Output: 10 -> 20 -> 30 -> NULL
+
+//     return 0;
+// }
+
+//SHOW THE  Addresses
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void insertAtTail(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    // Function to display data and addresses
+    void displayWithAddress() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << "Data: " << temp->data << ", Address: " << temp 
+                 << ", Next Address: " << temp->next << endl;
+            temp = temp->next;
+        }
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.insertAtTail(10);
+    list.insertAtTail(20);
+    list.insertAtTail(30);
+
+    list.displayWithAddress(); // Prints data and address of each node
+
+return 0;
 }
